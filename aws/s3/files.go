8152d564d350d086c19a643c9ad6400177869374
Stella <developer.ssu.ssu@gmail.com>
2026-01-28 08:25:55 +0000

    2. AWS Setting
    - configuration File

---
+++ aws/s3/files.go
@@ -0,0 +1,86 @@
+package s3
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// TransferObjectIfNotExist 는 배포 경로에 파일이 없다면 임시 경로에서 복사해옵니다.
+func (s *S3Service) TransferObjectIfNotExist(ctx context.Context, filename string) error {
+	destKey := fmt.Sprintf("%s/%s", s.config.MediaPrefix, filename)
+
+	// 1. 파일 존재 여부 확인 (HeadObject가 GetObject보다 비용이 저렴하고 효율적입니다)
+	headInput := &s3.HeadObjectInput{
+		Bucket: aws.String(s.config.Bucket),
+		Key:    aws.String(destKey),
+	}
+
+	_, err := s.svc.HeadObjectWithContext(ctx, headInput)
+	if err == nil {
+		// 파일이 이미 존재함
+		return nil
+	}
+
+	// 2. 파일이 없을 경우 CopyObject 수행 (기존 TransferObject 로직을 내재화)
+	sourceKey := fmt.Sprintf("%s/%s/%s", s.config.Bucket, s.config.TempPrefix, filename)
+
+	copyInput := &s3.CopyObjectInput{
+		Bucket:     aws.String(s.config.Bucket),
+		CopySource: aws.String(sourceKey),
+		Key:        aws.String(destKey),
+	}
+
+	_, err = s.svc.CopyObjectWithContext(ctx, copyInput)
+	if err != nil {
+		return fmt.Errorf("failed to transfer object from %s to %s: %w", sourceKey, destKey, err)
+	}
+
+	return nil
+}
+
+// ParseImgSrc 텍스트에디터에서 로컬파일url을 S3url로 바꾸고 파일도 복사하기
+func (s *S3Service) ParseImgSrc(content *string, prefix string) (*string, error) {
+	if content == nil {
+		return nil, errors.New("content empty")
+	}
+	re := regexp.MustCompile(`src="([^"]+)"[^>]*>`)
+	newContent := *content
+	for _, match := range re.FindAllStringSubmatch(*content, -1) {
+		key := match[1]
+		key = strings.ReplaceAll(key, s.config.CdnUri+"/", "")
+		key = filepath.Base(key)
+		bucket := s.config.Bucket
+		source := filepath.Join(bucket, s.config.TempPrefix, key)
+		dest := filepath.Join(s.config.MediaPrefix, prefix, key)
+		err := s.TransferObject(bucket, source, dest)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		newSrc := fmt.Sprintf("%s/%s", s.config.CdnUri, dest)
+		newContent = strings.ReplaceAll(newContent, match[1], newSrc)
+	}
+	return &newContent, nil
+}
+
+// TransferObject 임시저장소에 있는 오브젝트를 배포저장소로 복사하기
+func (s *S3Service) TransferObject(bucket string, source string, dest string) error {
+	input := &s3.CopyObjectInput{
+		Bucket:     aws.String(bucket),
+		Key:        aws.String(dest),
+		CopySource: aws.String(source),
+	}
+	_, err := s.svc.CopyObject(input)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return err
+}
--- go.mod
+++ go.mod
@@ -4,14 +4,27 @@ go 1.24.4
 
 require (
 	github.com/99designs/gqlgen v0.17.76
+	github.com/aws/aws-sdk-go v1.55.8
 	github.com/doug-martin/goqu/v9 v9.19.0
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/knadh/koanf/parsers/yaml v1.1.0
+	github.com/knadh/koanf/providers/env v1.1.0
+	github.com/knadh/koanf/providers/file v1.2.1
+	github.com/knadh/koanf/v2 v2.3.2
 	github.com/lib/pq v1.10.9
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/knadh/koanf/maps v0.1.2 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/vektah/gqlparser/v2 v2.5.30 // indirect
+	go.yaml.in/yaml/v3 v3.0.3 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 )
