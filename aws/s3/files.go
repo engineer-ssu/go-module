ac6eeb94fb94e24ae6e94fd1f08667b5e9e4f370
Stella <developer.ssu.ssu@gmail.com>
2026-01-29 02:15:59 +0000

    2

--- aws/s3/files.go
+++ aws/s3/files.go
@@ -3,9 +3,13 @@ package s3
 import (
 	"context"
 	"fmt"
+	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/engineer-ssu/go-module/config"
 )
 
 // TransferObjectIfNotExist 는 배포 경로에 파일이 없다면 임시 경로에서 복사해옵니다.
@@ -39,4 +43,42 @@ func (s *S3Service) TransferObjectIfNotExist(ctx context.Context, filename strin
 	}
 
 	return nil
+}
+
+// ParseImgSrc 텍스트에디터에서 로컬파일url을 S3url로 바꾸고 파일도 복사하기
+func (s *S3Service) ParseImgSrc(cfg config.Config, content *string, prefix string) *string {
+	if content == nil {
+		return nil
+	}
+	re := regexp.MustCompile(`src="([^"]+)"[^>]*>`)
+	newContent := *content
+	for _, match := range re.FindAllStringSubmatch(*content, -1) {
+		key := match[1]
+		key = strings.ReplaceAll(key, cfg.String("cdn_uri")+"/", "")
+		key = filepath.Base(key)
+		source := filepath.Join(cfg.String("s3_bucket"), cfg.String("s3_temp_prefix"), key)
+		dest := filepath.Join("media", prefix, key)
+		err := s.TransferObject(cfg, source, dest)
+		if err != nil {
+			fmt.Println(err)
+		}
+		newSrc := fmt.Sprintf("%s/%s", cfg.String("cdn_uri"), dest)
+		newContent = strings.ReplaceAll(newContent, match[1], newSrc)
+	}
+	return &newContent
+}
+
+// TransferObject 임시저장소에 있는 오브젝트를 배포저장소로 복사하기
+func (s *S3Service) TransferObject(cfg config.Config, source string, dest string) error {
+	input := &s3.CopyObjectInput{
+		Bucket:     aws.String(cfg.String("s3_bucket")),
+		Key:        aws.String(dest),
+		CopySource: aws.String(source),
+	}
+	_, err := s.svc.CopyObject(input)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return err
 }
