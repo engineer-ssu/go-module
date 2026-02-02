ddab3085a3f8f2669eea87053051245bf80f862f
Stella <developer.ssu.ssu@gmail.com>
2026-02-02 06:03:41 +0000

    1

--- aws/s3/files.go
+++ aws/s3/files.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/machinerd/go-module/idgen"
+	"github.com/engineer-ssu/go-module/idgen"
 )
 
 func (s *S3Service) TransferObjectIfNotExist(filename string) error {
